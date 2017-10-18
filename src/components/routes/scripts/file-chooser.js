bf6a2163714aa510483cb3ad4ffd7a1a37e7bf7a
Brian Ewing <brianewing@gmail.com>
2017-10-18 13:39:20 +0100

    File chooser: style tweaks, create a file

--- src/components/routes/scripts/file-chooser.js
+++ src/components/routes/scripts/file-chooser.js
@@ -1,6 +1,8 @@
 import { h, Component } from 'preact';
 import style from './style';
 
+import GoPlus from 'react-icons/go/plus';
+
 export default class FileChooser extends Component {
 	state = {
 		currentPath: '/'
@@ -18,22 +20,28 @@ export default class FileChooser extends Component {
 		})
 	}
 
+	createFile = () => {
+		let { webDavFs } = this.props
+		let { currentPath } = this.state
+		let name = prompt('Choose a file name')
+		let newFile = webDavFs.file(currentPath + name)
+		this.chooseFile(newFile)
+	}
+
 	chooseFile = (f) => {
 		this.props.onChoose && this.props.onChoose(f)
 	}
 
 	render({}, { files }) {
 		return <div class={style.fileChooser}>
-			<h1>File Chooser..</h1>
+			<h1>Choose a File.. <GoPlus onClick={this.createFile} /></h1>
 			{files ? this.renderFileList(files) : <i>Nothing to show</i>}
 		</div>
 	}
 
 	renderFileList(files) {
 		return	<ul>
-			{files.map((f) => <li onClick={this.chooseFile.bind(this, f)}>
-				{f.name}
-			</li>)}
+			{files.map((f) => <li onClick={() => this.chooseFile(f)}>{f.name}</li>)}
 		</ul>
 	}
 }
