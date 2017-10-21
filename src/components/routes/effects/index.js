9ef2de28acfdb94a488137346fd6fb82491050dd
Brian Ewing <brianewing@gmail.com>
2017-10-22 00:34:42 +0100

    effects: Rendering nicely

--- src/components/routes/effects/index.js
+++ src/components/routes/effects/index.js
@@ -3,16 +3,28 @@ import linkState from 'linkstate';
 
 import { SortableContainer, SortableElement, arrayMove } from 'react-sortable-hoc';
 
-import GoEllipsis from 'react-icons/lib/go/ellipsis';
+import FaAutomobile from 'react-icons/lib/fa/automobile';
+import FaCab from 'react-icons/lib/fa/cab';
+import FaClone from 'react-icons/lib/fa/clone';
+import FaCircleThin from 'react-icons/lib/fa/circle-thin';
+import FaCode from 'react-icons/lib/fa/code';
+import FaEraser from 'react-icons/lib/fa/eraser';
+import FaEllipsisH from 'react-icons/lib/fa/ellipsis-h';
+import FaLightbulbO from 'react-icons/lib/fa/lightbulb-o';
+import FaSquareO from 'react-icons/lib/fa/square-o';
+
+import MdBrightness5 from 'react-icons/lib/md/brightness-5';
+import MdLooks from 'react-icons/lib/md/looks';
 
 import style from './style';
 
 export default class Effects extends Component {
+	state = {
+		effects: []
+	}
+
 	componentWillReceiveProps(props) {
-		let { effects } = this.state
-		if(!effects && props.effects) {
-			this.setState({ effects: props.effects })
-		}
+		this.setState({ effects: props.effects })
 	}
 
 	send = () => this.props.onSend && this.props.onSend(this.state.effects)
@@ -24,7 +36,8 @@ export default class Effects extends Component {
 
 	render({}, { effects }) {
 		return <div class={style.effects}>
-			{<List items={effects} onSortEnd={this.onSortEnd} />}
+			<h2>Effects</h2>
+			{<List items={effects} transitionDuration={0} onSortStart={this.onSortStart} onSortEnd={this.onSortEnd} />}
 		</div>
 	}
 }
@@ -33,7 +46,7 @@ export default class Effects extends Component {
 class List extends Component {
 	render({ items }) {
 		return <ul>
-			{items && items.map((value, index) => <Effect key={`item-${index}`} index={index} value={value} />)}
+			{items && items.map((value, index) => <Effect index={index} value={value} />)}
 		</ul>
 	}
 }
@@ -41,14 +54,31 @@ class List extends Component {
 @SortableElement
 class Effect extends Component {
 	render({ value }) {
-		return <li>
-			<GoEllipsis /> <strong>{value.Type}</strong>
-			{ " {" }
-				{Object.entries(value.Params).map(([key, value]) => {
-					return <span><strong> {key}</strong> {JSON.stringify(value)}</span>
-					// return <span><strong> {key}</strong> {this.renderValue(value, key)}</span>;
-				})}
-			{ " }" }
+		return <li class={style.effect}>
+			<div style="display:block">
+				{this.renderIcon(value)} <strong>{value.Type}</strong>
+			</div>
+			{Object.entries(value.Params).map(([key, value]) => this.renderValue(value, key))}
 		</li>
+	}
+
+	renderIcon(effect) {
+		switch(effect['Type']) {
+			case 'Brightness': return <MdBrightness5 />
+			case 'Clear': return <FaEraser />
+			case 'External': return <FaCode />
+			case 'LarsonEffect': return <FaCab />
+			case 'Layer': return <FaClone />
+			case 'RainbowEffect': return <MdLooks />
+			case 'Stripe': return <FaEllipsisH />
+			default: return <FaSquareO />
+		}
+	}
+
+	renderValue(value, key) {
+		if(key == 'Effects')
+			return <List items={value} />
+		else
+			return <span><strong> {key}</strong> {JSON.stringify(value)}</span>
 	}
 }
