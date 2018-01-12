55d59f055333caa677a58edc0d5f3e984da305f5
Brian Ewing <brianewing@gmail.com>
2018-01-12 23:58:54 +0000

    File chooser: add loading state

--- src/components/routes/scripts/file-chooser.js
+++ src/components/routes/scripts/file-chooser.js
@@ -5,7 +5,8 @@ import GoPlus from 'react-icons/go/plus';
 
 export default class FileChooser extends Component {
 	state = {
-		currentPath: '/'
+		currentPath: '/',
+		files: null
 	}
 
 	componentWillMount() {
@@ -15,6 +16,7 @@ export default class FileChooser extends Component {
 	fetchFiles = () => {
 		let { webDavFs } = this.props
 		let { currentPath } = this.state
+		this.setState({ files: null })
 		webDavFs.dir(currentPath).children((files) => {
 			if(String.prototype.localeCompare) {
 				files.sort((a, b) => a.name.localeCompare(b.name))
@@ -37,14 +39,14 @@ export default class FileChooser extends Component {
 
 	render({}, { files }) {
 		return <div class={style.fileChooser}>
-			<h1>Choose a File.. <GoPlus onClick={this.createFile} /></h1>
-			{files ? this.renderFileList(files) : <i>Nothing to show</i>}
+			<h2>Choose a File.. <GoPlus onClick={this.createFile} /></h2>
+			{files == null ? <i>Fetching...</i> : this.renderFileList(files)}
 		</div>
 	}
 
 	renderFileList(files) {
-		return	<ul>
-			{files.map((f) => <li onClick={() => this.chooseFile(f)}>{f.name}</li>)}
-		</ul>
+		return (files.length == 0
+			? <i>Nothing to show</i>
+			: <ul>{files.map((f) => <li onClick={() => this.chooseFile(f)}>{f.name}</li>)}</ul>)
 	}
 }
